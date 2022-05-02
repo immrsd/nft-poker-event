d96b46899e44be69112cceacfe7e313d7186f059
immrsd <immrsd.eth@gmail.com>
2022-05-02 20:01:48 +0300

    Update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,26 +1,14 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-// When running the script with `hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from 'hardhat';
 import { Contract, ContractFactory } from 'ethers';
 
 async function main(): Promise<void> {
-  // Hardhat always runs the compile task when running scripts through it.
-  // If this runs in a standalone fashion you may want to call compile manually
-  // to make sure everything is compiled
-  // await run("compile");
-  // We get the contract to deploy
-  const PokerGameFactory: ContractFactory = await ethers.getContractFactory(
-    'PokerGame',
-  );
-  const pokerGame: Contract = await PokerGameFactory.deploy();
-  await pokerGame.deployed();
-  console.log('PokerGame deployed to: ', pokerGame.address);
+  const contractName = 'Tournament';
+  const factory: ContractFactory = await ethers.getContractFactory(contractName);
+  const tournament: Contract = await factory.deploy();
+  await tournament.deployed();
+  console.log(`${contractName} deployed to ${tournament.address}!`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error: Error) => {
