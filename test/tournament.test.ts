19201aa99719df6639155bb5eaba1826ad06c221
immrsd <immrsd.eth@gmail.com>
2022-05-28 14:35:08 +0300

    Add tests for initial contract state

--- test/tournament.test.ts
+++ test/tournament.test.ts
@@ -8,24 +8,45 @@ import { assert } from "console";
 chai.use(solidity);
 const { expect } = chai;
 
+const parseEther = ethers.utils.parseEther;
+const keccak256 = ethers.utils.keccak256;
+
+const Defaults = {
+  fee: parseEther("0.02"),
+  rake: 10,
+  seedCheckhash: keccak256(keccak256("Poker Tournament")),
+  duration: 21 * 24 * 60 * 60,
+  maxEntries: 50
+}
+
+const deployContract = async (
+  fee: BigNumber = Defaults.fee, 
+  rake: number = Defaults.rake, 
+  hash: string = Defaults.seedCheckhash
+): Promise<string> => {
+
+  const [deployer] = await ethers.getSigners();
+  const factory = new Tournament__factory(deployer);
+  const instance = await factory.deploy(fee, rake, hash);
+  return instance.address;
+}
+
 describe("Token", () => {
-  let tournamentAddress: string;
 
-  beforeEach(async () => {
-    const [deployer] = await ethers.getSigners();
-    const factory = new Tournament__factory(deployer);
-    const instance = await factory.deploy();
-    tournamentAddress = instance.address;
+  describe("Check initial state", async () => {
+    const address = await deployContract();
+    const [, user] = await ethers.getSigners();
+    const userInstance = new Tournament__factory(user).attach(address);
 
-    expect(await instance.totalSupply()).to.eq(0);
-  });
+    it("Should has correct init parameters", async () => {
+      expect(await userInstance.ENTRANCE_FEE()).to.be.equal(Defaults.fee, "Wrong fee value");
+      expect(await userInstance.RAKE_PERCENTAGE()).to.be.equal(Defaults.rake, "Wrong rake value");
+      expect(await userInstance.SEED_CHECKHASH()).to.be.equal(Defaults.seedCheckhash, "Wrong checkhash value");
+    });
 
-  describe("Enroll", async () => {
-    it("", async () => {
-      const [deployer, user] = await ethers.getSigners();
-      const instance = new Tournament__factory(user).attach(tournamentAddress);
-
-      await instance.enroll();
+    it("Should has correct constants", async () => {
+      expect(await userInstance.TOURNAMENT_DURATION()).to.be.equal(Defaults.duration, "Wrong duration");
+      expect(await userInstance.MAX_ENTRIES_PER_PLAYER()).to.be.equal(Defaults.maxEntries, "Wrong max entries");
     });
   });
 });
